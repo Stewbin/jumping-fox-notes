6e1130b49f5b77a21f1369adadc5edf392576b78
Akhil Paulson <akhilpaulson21@gmail.com>
2025-04-03 20:48:44 -0400

    Add login/signup components and server setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,21 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min";
-import "./styles/App.css";
-import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Home from './pages/Home';  // Import Home component
+import 'bootstrap/dist/css/bootstrap.css';
 
-function App() {
-  return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-    </Routes>
-  );
-}
+const App = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Login />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/home" element={<Home />} />  // Added Home route
+            </Routes>
+        </BrowserRouter>
+    );
+};
 
 export default App;
