3fb61f6b336715993b051b64920cf06c17bd4fd8
Akhil Paulson <akhilpaulson21@gmail.com>
2025-04-05 14:36:10 -0400

    dependency changes

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,9 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
-import Home from './pages/Home';  // Import Home component
+import Home from './pages/Home'; 
 import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
 
 const App = () => {
     return (
@@ -12,7 +13,7 @@ const App = () => {
                 <Route path="/" element={<Login />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
-                <Route path="/home" element={<Home />} />  // Added Home route
+                <Route path="/home" element={<Home />} />  {/* Added Home route */}
             </Routes>
         </BrowserRouter>
     );
