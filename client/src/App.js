c8d3d663f28e398730c7edffc31fa6dfc492a5b6
OnariRomain1 <oromain@albany.edu>
2025-04-09 14:01:25 -0400

    Removed the duplicate folder

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './components/Login';
 import Signup from './components/Signup';
 import Home from './pages/Home'; 
+import DrawingEditor from "./DrawingEditor"
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.js';
 
@@ -14,6 +15,8 @@ const App = () => {
                 <Route path="/login" element={<Login />} />
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/home" element={<Home />} />  {/* Added Home route */}
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/DrawingEditor" element={<DrawingEditor />} />
             </Routes>
         </BrowserRouter>
     );
